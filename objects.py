13b1f332abd3410b0eb3bde5093f5e2b9f9c394d
Pavel <maslov.pa@pystech.edu>
2022-12-20 01:03:54 +0300

    Переместил сюда класс Pow и Explosion

--- objects.py
+++ objects.py
@@ -1,8 +1,29 @@
 import pygame
-from images import volume_mixer
+from images import volume_mixer, explosion_anim, powerup_images, BLACK
+from settings import HEIGHT
+import random
+
+
+class Pow(pygame.sprite.Sprite):
+    """Класс апгрейдов"""
+    def __init__(self, center):
+        pygame.sprite.Sprite.__init__(self)
+        self.type = random.choice(['shield', 'gun'])
+        self.image = powerup_images[self.type]
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.center = center
+        self.speedy = 2
+
+    def update(self):
+        self.rect.y += self.speedy
+
+        if self.rect.top > HEIGHT:
+            self.kill()
 
 
 class Mixer(pygame.sprite.Sprite):
+    """Класс показателей громкости"""
     def __init__(self, x, y, state):
         pygame.sprite.Sprite.__init__(self)
 
@@ -20,3 +41,28 @@ class Mixer(pygame.sprite.Sprite):
 
         self.rect = self.image.get_rect()
         self.rect.center = center
+
+
+class Explosion(pygame.sprite.Sprite):
+    def __init__(self, center, size):
+        pygame.sprite.Sprite.__init__(self)
+        self.size = size
+        self.image = explosion_anim[self.size][0]
+        self.rect = self.image.get_rect()
+        self.rect.center = center
+        self.frame = 0
+        self.last_update = pygame.time.get_ticks()
+        self.frame_rate = 50
+
+    def update(self):
+        now = pygame.time.get_ticks()
+        if now - self.last_update > self.frame_rate:
+            self.last_update = now
+            self.frame += 1
+            if self.frame == len(explosion_anim[self.size]):
+                self.kill()
+            else:
+                center = self.rect.center
+                self.image = explosion_anim[self.size][self.frame]
+                self.rect = self.image.get_rect()
+                self.rect.center = center
