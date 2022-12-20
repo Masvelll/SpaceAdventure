169dbd5fb5cf525744df10f2614eed77393f761e
Pavel <maslov.pa@pystech.edu>
2022-12-20 13:13:09 +0300

    Сделал методы статическими, т.к. self там не используется

--- spawn_manager.py
+++ spawn_manager.py
@@ -1,4 +1,3 @@
-import pygame
 from music_manager import MusicManager
 from mob import Mob
 from enemy import Enemy, Boss
@@ -18,15 +17,16 @@ class SpawnManager:
             game.all_sprites.add(m)
             game.mobs.add(m)
 
-    def spawn_enemy(self, game):
+    @staticmethod
+    def spawn_enemy(game):
         """Создаёт врага"""
         m = Enemy(game.all_sprites, game.enemy_bullets)
         game.all_sprites.add(m)
         game.enemies.add(m)
 
-    def spawn_boss(self, game):
+    @staticmethod
+    def spawn_boss(game):
         """Создаёт босса"""
         m = Boss(game.all_sprites, game.boss_bullets)
         game.all_sprites.add(m)
         game.boss.add(m)
-
